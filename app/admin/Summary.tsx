1e92a2b5278079f3727179de83125cd9459ffd50
Harlloh <alloolorunfemi@gmail.com>
2024-02-09 00:04:29 +0100

    fixed the summary page

--- app/admin/Summary.tsx
+++ app/admin/Summary.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import Heading from "../components/Heading";
 import { formatPrice } from "../utils/formatPrice";
@@ -7,6 +7,7 @@ import { wishProductType } from "../wishlist/ItemContent";
 import useGetProducts from "@/hooks/useGetProducts";
 import useGetAllOrders from "@/hooks/getOrders";
 import useGetAllUsers from "@/hooks/useGetAllUser";
+import useGetAllStats from "@/hooks/useGetAllStats";
 
 type SummaryDataType = {
   [key: string]: {
@@ -14,11 +15,22 @@ type SummaryDataType = {
     digit: number;
   };
 };
+interface EndpointResponse {
+  totalSales: number;
+  totalProducts: number;
+  inStockProduct: number;
+  outOfStockProducts: number;
+  totalOrders: number;
+  totalUsers: number;
+}
 
 const Summary = () => {
   const { productss, loading, error, fetchProducts } = useGetProducts();
-  const {orders} = useGetAllOrders();
+  const { orders } = useGetAllOrders();
+  const { orderStat } = useGetAllStats();
   const { users, loadings, errors, fetchUsers } = useGetAllUsers();
+  const [orderStatistics, setOrderStats] =
+    useState<EndpointResponse>(orderStat);
   const [summaryData, setSummaryData] = useState<SummaryDataType>({
     sale: {
       label: "Total Sale",
@@ -56,45 +68,73 @@ const Summary = () => {
 
   const isLoading = loading || loadings;
 
-      useEffect(()=>{
+  // useEffect(() => {
+  //   setSummaryData((prev) => {
+  //     let tempData = { ...prev };
+  //     const totalSale = orders.reduce((accumulator, item: any) => {
+  //       if (item.status === "complete") {
+  //         return accumulator + item.amount;
+  //       } else return accumulator;
+  //     }, 0);
 
-        setSummaryData((prev) => {
-          let tempData = { ...prev };
-          const totalSale = orders.reduce((accumulator, item:any) => {
-            if (item.status === "complete") {
-              return accumulator + item.amount;
-            } else return accumulator;
-          }, 0);
+  //     const paidOrders = orders.filter((order: any) => {
+  //       return order.status === "complete";
+  //     });
 
-          const paidOrders = orders.filter((order:any) => {
-            return order.status === "complete";
-          });
+  //     const unPaidOrders = orders.filter((order: any) => {
+  //       return order.status === "pending";
+  //     });
+  //     const totalProducts = productss.length;
+  //     const inStockProducts = productss.filter(
+  //       (product: any) => product.inStock
+  //     ).length;
+  //     const outOfStockProducts = totalProducts - inStockProducts;
 
-          const unPaidOrders = orders.filter((order:any) => {
-            return order.status === "pending";
-          });
-          const totalProducts = productss.length;
-          const inStockProducts = productss.filter((product:any) => product.inStock).length;
-          const outOfStockProducts = totalProducts - inStockProducts;
+  //     tempData.sale.digit = totalSale;
+  //     tempData.orders.digit = orders.length;
+  //     tempData.paidOrders.digit = paidOrders.length;
+  //     tempData.unPaidOrders.digit = unPaidOrders.length;
+  //     tempData.products.digit = productss.length;
+  //     tempData.inStockProducts.digit = inStockProducts;
+  //     tempData.outOfStockProducts.digit = outOfStockProducts;
+  //     tempData.users.digit = users.length;
 
+  //     return tempData;
+  //   });
+  //   console.log(orderStat, orderStat);
+  // }, [orders, productss, users, orderStat]);
 
-          tempData.sale.digit = totalSale;
-          tempData.orders.digit = orders.length;
-          tempData.paidOrders.digit = paidOrders.length;
-          tempData.unPaidOrders.digit = unPaidOrders.length;
-          tempData.products.digit = productss.length;
-          tempData.inStockProducts.digit = inStockProducts;
-          tempData.outOfStockProducts.digit = outOfStockProducts;
-          tempData.users.digit = users.length;
-
-          return tempData;
-        });
-     
-      },[orders, productss, users])
-    
-
-
-
+  useEffect(() => {
+    if (orderStat) {
+      setSummaryData({
+        sale: {
+          label: "Total Sale",
+          digit: orderStat.totalSales || 0,
+        },
+        products: {
+          label: "Total Products",
+          digit: orderStat.totalProducts || 0,
+        },
+        inStockProducts: {
+          label: "In Stock",
+          digit: orderStat.inStockProduct || 0,
+        },
+        outOfStockProducts: {
+          label: "Out of Stock",
+          digit: orderStat.outOfStockProducts || 0,
+        },
+        orders: {
+          label: "Total Orders",
+          digit: orderStat.totalOrders || 0,
+        },
+        users: {
+          label: "Total Users",
+          digit: orderStat.totalUsers || 0,
+        },
+      });
+    }
+    console.log(summaryData, ">>>>>>>");
+  }, [orderStat]);
   const summaryKeys = Object.keys(summaryData);
   return (
     <div className="max-w-[1150px] m-auto">
---
+++ hooks/useGetAllStats.tsx
@@ -0,0 +1,42 @@
+import { BASE_URL } from "@/api/auth/apis";
+import axios from "axios";
+import { useEffect, useState } from "react";
+interface EndpointResponse {
+  totalSales: number;
+  totalProducts: number;
+  inStockProduct: number;
+  outOfStockProducts: number;
+  totalOrders: number;
+  totalUsers: number;
+}
+
+const useGetAllStats = () => {
+  const [orderStat, setOrderStat] = useState<EndpointResponse | any>([]);
+  const [loadings, setLoading] = useState(true);
+  const [errors, setError] = useState(null);
+  const userToken = localStorage.getItem("user");
+  const fetchStats = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(`${BASE_URL}/admin/statistics`, {
+        headers: {
+          Authorization: userToken,
+        },
+      });
+      console.log(res, "hellloooo");
+
+      setOrderStat(res.data.data);
+    } catch (error: any) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchStats();
+  }, []);
+
+  return { orderStat, loadings, errors, fetchStats };
+};
+
+export default useGetAllStats;
