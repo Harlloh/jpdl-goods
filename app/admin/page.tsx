c9bf5ad8ef10c88c1c5d1c4b726ec376efae0d86
Harlloh <alloolorunfemi@gmail.com>
2024-02-10 15:39:21 +0100

    deploy bug

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,21 +3,15 @@ import Summary from "./Summary";
 import Container from "../components/Container";
 import useGetProducts from "@/hooks/useGetProducts";
 import useGetAllUsers from "@/hooks/useGetAllUser";
-import { getOrders } from "@/hooks/getOrders";
-import BarGraph from "./BarGraph";
 
 async function page() {
   // const products = await getProducts({ category: null });
- 
-
 
   return (
     <div className="pt-8">
       <Container>
-        <Summary/>
-        <div className="mt-4 mx-auto max-w-[1150px]">
-          {/* <BarGraph  /> */}
-        </div>
+        <Summary />
+        <div className="mt-4 mx-auto max-w-[1150px]">{/* <BarGraph  /> */}</div>
       </Container>
     </div>
   );
