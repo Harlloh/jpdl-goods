49c872b64f7edc3b05ed72828a8e0e78a1616975
Harlloh <alloolorunfemi@gmail.com>
2024-02-10 17:26:53 +0100

    deployment issue

--- api/auth/apis.ts
+++ api/auth/apis.ts
@@ -1,8 +1,15 @@
 export const BASE_URL = "https://store-api-pyo1.onrender.com";
 
 export const getToken = () => {
-  return localStorage.getItem("user");
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("user");
+  }
+  return null; // or handle the absence of window.localStorage in your desired way
 };
+
 export const getIsAdmin = () => {
-  return localStorage.getItem("isAdmin");
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("isAdmin");
+  }
+  return null; // or handle the absence of window.localStorage in your desired way
 };
